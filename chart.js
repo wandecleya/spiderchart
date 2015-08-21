4189d98dfe4e54d40105e8dd23b00486d87484f0
wandecleya <wandecleya@gmail.com>
2015-08-21 17:00:31 -0300

    Draws polygons

--- chart.js
+++ chart.js
@@ -4,26 +4,59 @@ function baseChart() {
 
 	if(canvas.getContext){
 		var context = canvas.getContext("2d");
-		var angle = Math.PI/2;
-		var length = 300;
-		var xCoordinate = 100;
-		var yCoordinate = 100;
+		var numberOfSides = 6;
+		var radius = 200;
+		var initialCoordinates = [100, 100];
+
+		var vertices = verticesCoordinates(initialCoordinates, numberOfSides, radius);
 
 		context.beginPath();
-		context.moveTo(xCoordinate, yCoordinate);
-		coordinates = calculatesEndingCoordinates(xCoordinate, yCoordinate, angle, length);
-		context.lineTo(coordinates.x, coordinates.y);
+		context.moveTo(vertices[0][0], vertices[0][1]);
+
+		for (var i=1; i<vertices.length; i++){
+			
+			context.lineTo(vertices[i][0], vertices[i][1]);
+			context.stroke();
+			context.beginPath();
+			context.moveTo(vertices[i][0], vertices[i][1]);
+		}
+
+		context.lineTo(vertices[0][0], vertices[0][1]);
 		context.stroke();
-
-
 	}
 }
 
 
 
-function calculatesEndingCoordinates(xInitial, yInitial, angle, length){
-	var coordinates = {};
-	coordinates["x"] = (length * Math.cos(angle)) + xInitial;
-	coordinates["y"] = (length * Math.sin(angle)) + yInitial;
+function nextCoordinates(initial, angle, length){
+	var coordinates = [];
+	coordinates[0] = (length * Math.cos(angle)) + initial[0];
+	coordinates[1] = (length * Math.sin(angle)) + initial[1];
 	return coordinates;
 }
+
+function polygonParameters(numberOfSides, radius){
+	var parameters = {};
+	parameters["angle"] = (2 * Math.PI )/ numberOfSides;
+	parameters["length"] = (2 * radius) * Math.sin(Math.PI/numberOfSides);
+	return parameters;
+}
+
+function verticesCoordinates(initial, numberOfSides, radius){
+	var coordinates = [];
+	var parameters = polygonParameters(numberOfSides, radius);
+	var angle = 0;
+
+	for(var i=0; i < numberOfSides; i++){
+		coordinates.push(initial);
+		var next = nextCoordinates(initial, angle, parameters.length);
+		initial = next;
+		angle += parameters.angle;
+	}
+	
+
+	return coordinates;
+}
+
+
+
--- jasmine-standalone-2.3.4/spec/ChartSpec.js
+++ jasmine-standalone-2.3.4/spec/ChartSpec.js
@@ -1,19 +1,53 @@
 describe("Spider Chart", function() {
 
 
-	it("Calculates Endding Coordinates of a Line from Starting Point, Length and Angle", function(){
-		var angle = Math.PI / 3;
-		var length = 200;
-		var xInitial = 50;
-		var yInitial = 50;
+	it("Calculates Next Coordinates of a Line from Starting Point, Length and Angle", function(){
+		var angle = 0;
+		var length = 20;
+		var initial = [10, 10];
 
-		var xCoordinate = (length * Math.cos(angle)) + xInitial;
-		var yCoordinate = (length * Math.sin(angle)) + yInitial;
+		var xCoordinate = (length * Math.cos(angle)) + initial[0];
+		var yCoordinate = (length * Math.sin(angle)) + initial[1];
 
-		var result = calculatesEndingCoordinates(xInitial, yInitial, angle, length);
+		var result = nextCoordinates(initial, angle, length);
 
-		expect(result["x"]).toBeCloseTo(xCoordinate, 0);
-		expect(result["y"]).toBeCloseTo(yCoordinate, 0);
+		expect(result[0]).toBeCloseTo(xCoordinate, 0);
+		expect(result[1]).toBeCloseTo(yCoordinate, 0);
+		expect(Math.cos(angle)).toBeCloseTo(1, 0);
+		expect(Math.sin(angle)).toBeCloseTo(0, 0);
+		expect(result[0]).toBeCloseTo(30, 0);
+		expect(result[1]).toBeCloseTo(10, 0);
+	});
+
+	it("Calculates Polygon angle and length of the side from number os sides and radius", function(){
+		var numberOfSides = 4;
+		var radius = 10 * Math.sqrt(2);
+
+		var angle = (2 * Math.PI) / numberOfSides;
+		var sideLength = (2 * radius) * Math.sin(Math.PI / numberOfSides);
+
+		var result = polygonParameters(numberOfSides, radius);
+
+		expect(result["angle"]).toBeCloseTo(angle, 0);
+		expect(result["length"]).toBeCloseTo(sideLength, 0);
+	});
+
+	it("Calculates Vertices Coordinates of Polygon", function(){
+		var numberOfSides = 4;
+		var initial = [10, 10];
+
+		var radius = 10 * Math.sqrt(2);
+		var square = [[10, 10], [30, 10], [30, 30], [10, 30]];
+		var result = verticesCoordinates(initial, numberOfSides, radius);
+
+		expect(result[0][0]).toBeCloseTo(square[0][0]);
+		expect(result[0][1]).toBeCloseTo(square[0][1]);
+		expect(result[1][0]).toBeCloseTo(square[1][0]);
+		expect(result[1][1]).toBeCloseTo(square[1][1]);
+		expect(result[2][0]).toBeCloseTo(square[2][0]);
+		expect(result[2][1]).toBeCloseTo(square[2][1]);
+		expect(result[3][0]).toBeCloseTo(square[3][0]);
+		expect(result[3][1]).toBeCloseTo(square[3][1]);
 	});
 
 });
