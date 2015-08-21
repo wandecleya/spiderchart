8829c301570eede8903162216615f8595952c2d9
wandecleya <wandecleya@gmail.com>
2015-08-21 12:48:46 -0300

    Calculates line coodinates from starting point, length and angle

--- chart.js
+++ chart.js
@@ -4,18 +4,28 @@ function baseChart() {
 
 	if(canvas.getContext){
 		var context = canvas.getContext("2d");
+		var angle = Math.PI/2;
+		var length = 300;
+		var xCoordinate = 100;
+		var yCoordinate = 100;
 
+		context.beginPath();
+		context.moveTo(xCoordinate, yCoordinate);
+		xCoordinate = calculateX(xCoordinate, angle, length);
+		yCoordinate = calculateY(yCoordinate, angle, length);
+		context.lineTo(xCoordinate, yCoordinate);
+		context.stroke();
 
 
 	}
 }
 
-function calculateX(angle, length){
-	var x_coordinate = length * Math.cos(angle);
+function calculateX(xInitial, angle, length){
+	var x_coordinate = (length * Math.cos(angle)) + xInitial;
 	return x_coordinate;
 }
 
-function calculateY(angle, length){
-	var y_coordinate = length * Math.sin(angle);
+function calculateY(yInitial, angle, length){
+	var y_coordinate = (length * Math.sin(angle)) + yInitial;
 	return y_coordinate;
 }
--- jasmine-standalone-2.3.4/spec/ChartSpec.js
+++ jasmine-standalone-2.3.4/spec/ChartSpec.js
@@ -1,17 +1,19 @@
 describe("Spider Chart", function() {
-	it("Calculate Ending X Coordinate of a Line from an Angle and Length", function(){
+	it("Calculate Ending X Coordinate of a Line from an Angle and Length and X initial", function(){
 		var angle = Math.PI / 3;
 		var length = 200;
-		var xCoordinate = length * Math.cos(angle);
-		var result = calculateX(angle, length);
+		var xInitial = 50;
+		var xCoordinate = (length * Math.cos(angle)) + xInitial;
+		var result = calculateX(xInitial, angle, length);
 		expect(result).toBeCloseTo(xCoordinate, 0);
 	});
 
-		it("Calculate Ending Y Coordinate of a Line from an Angle and Length", function(){
+		it("Calculate Ending Y Coordinate of a Line from an Angle and Length and Y initial", function(){
 		var angle = Math.PI / 3;
 		var length = 200;
-		var yCoordinate = length * Math.sin(angle);
-		var result = calculateY(angle, length);
+		var yInitial = 50;
+		var yCoordinate = (length * Math.sin(angle)) + yInitial;
+		var result = calculateY(yInitial, angle, length);
 		expect(result).toBeCloseTo(yCoordinate, 0);
 	});
 
