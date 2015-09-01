3617fed37f4149145afe120dbab29a7a50a2ea5a
wandecleya <wandecleya@gmail.com>
2015-09-01 18:16:09 -0300

    Complete refactoring to Object Orientation

--- polygon.js
+++ polygon.js
@@ -1,4 +1,39 @@
 
+
+
+function baseChart() {
+	var canvas = document.getElementById("myCanvas");
+
+	if(canvas.getContext){
+		var context = canvas.getContext("2d");
+		var width = 800;
+		var hight = 400;
+
+		var radius = 100;
+		var numberOfSides = 12;
+		var center = new Point(400, 200);
+
+		var polygon = new Polygon(radius, numberOfSides, center);
+		
+
+
+		var vertices = polygon.generateVertices();
+
+
+		context.beginPath();
+		context.moveTo(vertices[0].x, vertices[0].y);
+
+		for (var i=1; i<vertices.length; i++){
+			
+			context.lineTo(vertices[i].x, vertices[i].y);
+			context.stroke();
+		}
+
+		context.lineTo(vertices[0].x, vertices[0].y);
+		context.stroke();
+	}
+}
+
 function Point(x, y, description){
 	this.x= x,
 	this.y= y,
@@ -10,7 +45,7 @@ function Polygon(radius, sides, center){
 	this.sides = sides,
 	this.center = center,
 	this.vertices = []
-	
+
 }
 
 Polygon.prototype.vertice = function(angle){
@@ -27,8 +62,7 @@ Polygon.prototype.generateVertices = function(){
 	for(var angle = 0; angle<= radiano; angle += internalAngle){
 		var point = this.vertice(angle);
 		this.vertices.push(point);
-	}
-	
+	}	
 	
 	return this.vertices;
 }
